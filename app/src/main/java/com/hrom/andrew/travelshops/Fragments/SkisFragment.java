bea75f7e7db5d77d583f1d4ec8e81699bf5bfc7a
Andrii Hrom <groshkka@gmail.com>
2015-07-17 00:10:54 +0300

    develop SkiFragment

--- app/src/main/java/com/hrom/andrew/travelshops/Fragments/SkisFragment.java
+++ app/src/main/java/com/hrom/andrew/travelshops/Fragments/SkisFragment.java
@@ -1,4 +1,43 @@
 package com.hrom.andrew.travelshops.Fragments;
 
-public class SkisFragment {
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SimpleAdapter;
+
+import com.hrom.andrew.travelshops.R;
+import com.hrom.andrew.travelshops.ShopDatas.SkisShop;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class SkisFragment extends ListFragment{
+    public final static String TAG = "Ski";
+    private SkisShop skisShop = new SkisShop();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        List<HashMap<String,String>> listBikeShop = new ArrayList<>();
+
+        for (int i = 0; i < skisShop.getListShops().size(); i++) {
+            HashMap<String,String> hm = new HashMap<>();
+
+            hm.put("img",Integer.toString(skisShop.getIconShops().get(i)));
+            hm.put("txt",skisShop.getListShops().get(i));
+            listBikeShop.add(hm);
+        }
+
+        String[] from = {"img","txt"};
+        int[] to = {R.id.imgForList,R.id.textForSnowboard};
+
+        SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(),listBikeShop,R.layout.list_for_snowboard,from,to);
+        setListAdapter(adapter);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
